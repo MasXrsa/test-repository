c63436a04adc48972a89e89d307dfaa8af138d96
MasXrsa <twinkmaximlove510@gmail.com>
2024-07-09 17:27:43 +0300

    add message handler

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-const { Telegraf } = require("telegraf")
+const { Telegraf } = require("telegraf");
+require("dotenv").config();
 
-const token = process.env.token
-const bot = new Telegraf(token)
+const token = process.env.TOKEN;
+const bot = new Telegraf(token);
+
+bot.on("message", async ctx =>{
+    const msg = ctx.message.text;
+
+    if(msg == "ключ"){
+        ctx.reply("Key word");
+    }
+})
+
+bot.launch();
+console.log("Bot start");
