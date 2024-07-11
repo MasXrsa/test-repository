67801767c3dc8c0ca4fdb03771ffca01cdb207d8
MasXrsa <twinkmaximlove510@gmail.com>
2024-07-11 20:58:28 +0300

    add dependences

--- bot.js
+++ bot.js
@@ -1,11 +1,15 @@
 const { Telegraf } = require("telegraf");
+const { start } = require("./controllers/command")
 require("dotenv").config({path: './config/.env'});
+
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 const setupBot = () =>{
     bot.use((ctx, next) => {
         console.log(ctx);
+        console.log(ctx.message.chat);
+        console.log(ctx.message.chat.id);
         return next()
 
     })
